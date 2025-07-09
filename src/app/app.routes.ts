64e2af46e4e5206eb96ddbacc116d78fc4ff22d0
Nathan WITTWER <144239056+nathming@users.noreply.github.com>
2025-07-09 13:18:11 +0200

    MVP

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { FormPageComponent } from './pages/form-page/form-page.component';
 import { ImagePageComponent } from './pages/image-page/image-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 export const routes: Routes = [
-  { path: '', component: FormPageComponent },
+  { path: '', component: HomePageComponent },
+  { path: 'form', component: FormPageComponent },
   { path: 'image', component: ImagePageComponent }
 ];
